08587f3121c63ade824d0da18421d9e2b240b78c
segi3channel <44390326+segi3channel@users.noreply.github.com>
2018-11-10 15:21:56 +0700

    Create confg.php

---
+++ confg.php
@@ -0,0 +1,33 @@
+<?php
+$android_id = "2d7c803f6277cf3a";
+$deviceCode = "866037030559723";
+$device_ip  = "116.206.15.48";
+$token = 
+"680atvthzwgzU03MhKnGujYs0hkGGGwvoJW8WbPeMTfkHRL_ecKneO3UNObXvNQm9MR91uBNKm7iezw";
+$tk = 
+"ACBvc-8tqVBNcojakKXdUMWErurEoXdbbI5xdHRodw";
+$uuid = 
+"5e9129e17b5b4398a73cf23326abc90d";
+$userid = "3953206";
+$sign = 
+"dda265401da5eb3c0a5cc18ca045e313";
+
+####################################################
+#|note:
+#| *untuk aktifitas nuyul multi akun
+#|  gunakan andoid_id yang berbeda setiap akun
+#|  gunakan devicecode (imei) yang berbeda setiap akun
+#|  gunakan device_ip yang berbeda setiap akun
+#| *untuk settingan lainya wajib sama 
+#|  dengan data dari packet capture
+#|
+####################################################
+#| cara mengaktifkan multi akun
+#| duplikat data di atas dari anroid_id => sign
+#| buat file baru dengan nama bebas terserah kamu
+#| aktifkan skrip di bawah dngn menghapus tanda pagar
+#| jika skrip di aktifkan maka hapus data di atas 
+#|#####################################################
+#| hapus pagar di bawah untuk mengaktifkan skrip
+
+# include(readline("masukan config: "));
